a181d54ad234cf18f63a0cdeb5c05099075ad236
Marc Majcher <majcher@gmail.com>
2018-08-09 13:53:26 -0500

    simple

--- binding.js
+++ binding.js
@@ -3,6 +3,17 @@
 /* eslint-env browser */
 
 (() => {
-    console.log('we are here');
+    const dataObj = {
+        firstName: '';
+    };
+    
+    const outputDiv = document.getElementById('nameHere');
 
+    function updateInput(event) {
+        console.log(event.target.value);
+        outputDiv.innerText = event.target.value;
+    }
+
+    const fnInput = document.getElementById('firstName');
+    fnInput.addEventListener('keydown', updateInput);
 })();
